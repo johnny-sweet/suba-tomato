959cb99360f97fbcf35f35e5b44c8b783886d2e0
j.suess <jonas.suess@tum.de>
2025-07-14 09:41:30 +0200

    Update timer durations and add new package dependency

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import time
 def main(working_time: Annotated[int, typer.Argument()], break_time: Annotated[int, typer.Argument()]):
     print(f"Working time is {working_time} minutes.")
     print(f"Break time is {break_time} minutes.")
-    for _ in track(range(working_time), description="working period..."):
-        time.sleep(5)
+    for _ in track(range(working_time * 2), description="working period..."):
+        time.sleep(30)
     
     # TODO system notification
     
     print("Time for a break!")
-    for _ in track(range(break_time), description="break period..."):
-        time.sleep(5)
+    for _ in track(range(break_time * 2), description="break period..."):
+        time.sleep(30)
         
     print("Break is over! You can restart the timer if you want.")
     restart = typer.confirm("Do you want to restart the timer?")
