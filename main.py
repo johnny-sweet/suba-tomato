150e317552f32f9ec8f2ebe07c95b9f5e2b5fb57
j.suess <jonas.suess@tum.de>
2025-07-14 09:34:54 +0200

    Add initial project files including .gitignore, .python-version, README, main.py, pyproject.toml, and uv.lock

---
+++ main.py
@@ -0,0 +1,27 @@
+import typer
+from typing_extensions import Annotated
+from rich.progress import track
+import time
+
+
+def main(working_time: Annotated[int, typer.Argument()], break_time: Annotated[int, typer.Argument()]):
+    print(f"Working time is {working_time} minutes.")
+    print(f"Break time is {break_time} minutes.")
+    for _ in track(range(working_time), description="working period..."):
+        time.sleep(5)
+    
+    # TODO system notification
+    
+    print("Time for a break!")
+    for _ in track(range(break_time), description="break period..."):
+        time.sleep(5)
+        
+    print("Break is over! You can restart the timer if you want.")
+    restart = typer.confirm("Do you want to restart the timer?")
+    if restart:
+        main(working_time, break_time)
+    else:
+        print("Goodbye!")
+
+if __name__ == "__main__":
+    typer.run(main)
