7fcf27de00912154736f29064d1106238a49789c
j.suess <jonas.suess@tum.de>
2025-07-14 10:55:47 +0200

    Refactor main function to enhance user prompts and improve progress display

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 import typer
 from typing_extensions import Annotated
 from rich.progress import track
+from rich.prompt import Confirm, Prompt
+from rich import print
 import time
+import winsound
 
+pomo_count = 0
 
 def main(working_time: Annotated[int, typer.Argument()], break_time: Annotated[int, typer.Argument()]):
+    global pomo_count
+
     print(f"Working time is {working_time} minutes.")
     print(f"Break time is {break_time} minutes.")
-    for _ in track(range(working_time * 2), description="working period..."):
+
+    for _ in track(range(working_time * 2), description="[bold yellow]working period[/bold yellow]..."):
         time.sleep(30)
-    
-    # TODO system notification
+
+    winsound.MessageBeep()
     
     print("Time for a break!")
-    for _ in track(range(break_time * 2), description="break period..."):
+    Prompt.ask("Press any key to start the break timer")
+    for _ in track(range(break_time * 2), description="[bold green]break period[/bold green]..."):
         time.sleep(30)
-        
+    
+    winsound.MessageBeep()    
     print("Break is over! You can restart the timer if you want.")
-    restart = typer.confirm("Do you want to restart the timer?")
+    restart = Confirm.ask("Do you want to restart the timer?")
+    pomo_count += 1
     if restart:
         main(working_time, break_time)
     else:
-        print("Goodbye!")
-
+        print(f"You completed {pomo_count} pomodoro interval{"s" if pomo_count > 1 else ""}!")
+      
 if __name__ == "__main__":
     typer.run(main)
