d5819bfce6dcfebd9729f5f8fb5fe916676aad3a
j.suess <jonas.suess@tum.de>
2025-07-15 17:53:32 +0200

    Add notification function to handle sound alerts for work and break periods

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ from rich.prompt import Confirm, Prompt
 from rich import print
 import time
 import winsound
+import sys
+import os
 
 pomo_count = 0
+
+def notify():
+    if sys.platform.startswith("win"):
+        import winsound
+        winsound.MessageBeep()
+    else:
+        print("\a", end="", flush=True)
 
 def main(working_time: Annotated[int, typer.Argument()], break_time: Annotated[int, typer.Argument()]):
     global pomo_count
@@ -17,14 +26,14 @@ def main(working_time: Annotated[int, typer.Argument()], break_time: Annotated[i
     for _ in track(range(working_time * 2), description="[bold yellow]working period[/bold yellow]..."):
         time.sleep(30)
 
-    winsound.MessageBeep()
+    notify()
     
     print("Time for a break!")
     Prompt.ask("Press enter to start the break timer")
     for _ in track(range(break_time * 2), description="[bold green]break period[/bold green]..."):
         time.sleep(30)
     
-    winsound.MessageBeep()    
+    notify()    
     print("Break is over! You can restart the timer if you want.")
     restart = Confirm.ask("Do you want to restart the timer?")
     pomo_count += 1
